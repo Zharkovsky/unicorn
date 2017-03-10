852aff44ce246d0246b6796b40e08330c3abff4e
Nikita <kit.00@list.ru>
2017-03-10 22:40:37 +0300

    Add sum function

--- functions.cpp
+++ functions.cpp
@@ -21,3 +21,14 @@ void show(float **a, int n,int m)
     }
 }
 
+double **Sum(double **A,double **B,int An,int Am,int Bn,int Bm)
+{
+    if(An!=Bn || Am!=Bm) return NULL;
+    double **C = new double *[An];
+    for(int i=0;i<Am;i++)
+        C[i] = new double [Am];
+    for(int i=0;i<An;i++)
+        for(int j=0;j<Am;j++)
+            C[i][j]=A[i][j]+B[i][j];
+    return C;
+}
