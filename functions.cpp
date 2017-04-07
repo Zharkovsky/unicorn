ca31af6e6da0c1c189fabb4241857acd16b92fbf
Nikita Zharkovsky <kit.00@list.ru>
2017-04-07 21:33:11 +0300

    Tabulation

--- functions.cpp
+++ functions.cpp
@@ -6,105 +6,115 @@
 
 using namespace std;
 
-double **newmatrix (int line, int column)
+double **newMatrix (int line, int column)
 {
 	double **matrix = new double *[line];
-	for (int i = 0; i < line; i++)
-		matrix[i] = new double[column];
+	
+	for (int i = 0; i < line; ++i)
+		matrix[i] = new double [column];
+		
 	return matrix;
 }
 
-double **MulNum(double **A,int n,int m,double digit)
+double **mulNum (double **A, int n, int m, double digit)
 {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            A[i][j] *= digit;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+        	A[i][j] *= digit;
 
     return A;
 }
 
-void show(double **a, int n,int m)
+void show (double **a, int n, int m)
 {
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i)
     {
-        for(int j = 0; j < m; j++)
-            cout << setw(15) << a[i][j];
-        cout << endl;
+        for (int j = 0; j < m; ++j)
+        	cout << setw(15) << a[i][j];
+    	cout << endl;
     }
 }
 
 
-double Determinate(double **a,int size)
+double determinate (double **a, int size)
 {
- 	int i,j;
+ 	int i, j;
  	double det = 0;
  	double **matr;
+ 	
  	if (size == 1)
- 	{
- 	 	det = a[0][0];
+ 	 	det = a[0][0];	
+ 	else if (size == 2)
+		det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+	else
+	{
+  		matr = new double *[size-1];
+  		
+  		for (i = 0; i < size; ++i)
+  		{
+   			for (j = 0; j < size-1; ++j)
+   			{
+    			if (j < i)
+     			   	matr[j] = a[j];
+    			else
+     			    matr[j] = a[j+1];
+   			}
+   			det += pow ((double)-1, (i+j)) * determinate (matr, size-1) * a[i][size-1];
+  		}
+  		
+  		delete[] matr;
  	}
- 	else 
-	 	if (size == 2)
-		{
-			det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
-		}
-		else
-		{
-  			matr = new double*[size-1];
-  			for(i = 0; i < size; ++i)
-  			{
-   				for(j = 0; j < size-1; ++j)
-   				{
-    			   if (j < i)
-     			   	  matr[j] = a[j];
-    			   else
-     			      matr[j] = a[j+1];
-   				}
-   				det += pow((double)-1,(i+j)) * Determinate(matr, size-1) * a[i][size-1];
-  			}
-  			delete[] matr;
- 		}
-		return det;
+ 	
+	return det;
 }
 
-double **Sum(double **A,double **B,int An,int Am,int Bn,int Bm)
+double **sum (double **A, double **B, int An, int Am, int Bn, int Bm)
 {	
-    if(An != Bn || Am != Bm) 
+    if (An != Bn || Am != Bm) 
     {
-    	cout<<"Error. Try Again."<<endl;
-		double **C = new double*[An];
-		for(int i = 0; i < An; i++)
-        	C[i] = new double[Am];
-        for(int i = 0; i < An; i++)
-        	for(int j = 0; j < Am; j++)
+    	cout << "Error. Try Again." << endl;
+    	
+		double **C = new double *[An];
+		
+		for (int i = 0; i < An; ++i)
+        	C[i] = new double [Am];
+        	
+        for (int i = 0; i < An; ++i)
+        	for (int j = 0; j < Am; ++j)
             	C[i][j] = 0.0;
+            	
     	return C;
     }
     
     double **C = new double *[An];
-    for(int i = 0; i < An; i++)
-        C[i] = new double[Am];
+    
+    for (int i = 0; i < An; i++)
+        C[i] = new double [Am];
         
-    for(int i = 0; i < An; i++)
-        for(int j = 0; j < Am; j++)
+    for (int i = 0; i < An; ++i)
+        for (int j = 0; j < Am; ++j)
             C[i][j] = A[i][j] + B[i][j];
+            
     return C;
 }
 
-double **Trans(double **A,int n,int m)
+double **trans (double **A, int n, int m)
 {
-    double **B = new double*[m];
-    for(int i = 0; i < m; i++)
-        B[i] = new double[n];
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
+    double **B = new double *[m];
+    
+    for (int i = 0; i < m; ++i)
+        B[i] = new double [n];
+        
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
             B[j][i] = A[i][j];
+            
     return B;
 }
 
-double ** Multiplication(double **A, double **B, int lineA, int columnA, int lineB, int columnB)
+double **multiplication (double **A, double **B, int lineA, int columnA, int lineB, int columnB)
 {
-	double **result = newmatrix(lineA, columnB);
+	double **result = newMatrix(lineA, columnB);
 		
 	for (int i = 0; i < lineA; i++)
 		for (int j = 0; j < columnB; j++)
@@ -122,55 +132,68 @@ double ** Multiplication(double **A, double **B, int lineA, int columnA, int lin
 	return A;
 }*/
 
-void DelMatr(double **A, int lineA)
+void delMatr (double **A, int lineA)
 {
-     for (int i = 0; i < lineA; ++i)
-         delete[] A[i];
-     delete [] A;
+    for (int i = 0; i < lineA; ++i)
+    	delete[] A[i];
+    delete[] A;
 }
 
-void obrmatrix(double **A,int n) 
+void obrMatrix (double **A, int n) 
 {
-    double **matrix = new double*[n];
-    for(int i = 0; i < n; i++)
-       matrix[i] = new double[2*n];
-    for(int i = 0; i < n; i++)
-       for(int j = 0; j < n; j++)
-          matrix[i][j] = A[i][j];
+    double **matrix = new double *[n];
     
-    double ratio,x;
+    for (int i = 0; i < n; ++i)
+        matrix[i] = new double[2*n];
+       
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            matrix[i][j] = A[i][j];
+    
+    double ratio, x;
     int i, j, k;
-    for(i = 0; i < n; i++){
-        for(j = n; j < 2*n; j++){
-            if(i == (j-n))
+    
+    for (i = 0; i < n; ++i)
+	{
+        for (j = n; j < 2*n; ++j)
+		{
+            if (i == (j-n))
                 matrix[i][j] = 1.0;
             else
                 matrix[i][j] = 0.0;
         }
     }
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            if(i != j){
+    
+    for (i = 0; i < n; ++i)
+	{
+        for (j = 0; j < n; ++j)
+		{
+            if (i != j)
+			{
                 ratio = matrix[j][i]/matrix[i][i];
-                for(k = 0; k < 2*n; k++){
+                for (k = 0; k < 2*n; ++k)
+				{
                     matrix[j][k] -= ratio * matrix[i][k];
                 }
             }
         }
     }
-    for(i = 0; i < n; i++){
+    for (i = 0; i < n; ++i)
+	{
         x = matrix[i][i];
-        for(j = 0; j < 2*n; j++){
+        for (j = 0; j < 2*n; ++j)
+		{
             matrix[i][j] /= x;
         }
     }
-  //  printf("The inverse matrix is: \n");
+    
+    //printf("The inverse matrix is: \n");
     //show(matrix,n,2*n);
-    DelMatr(matrix,n);
-    return;
+    
+    delMatr (matrix, n);
 }    
     
-int rank(double ** matrix, int i, int j)
+int rank (double **matrix, int i, int j)
 {
     int r = 0;
     int q = 1;
@@ -181,129 +204,161 @@ int rank(double ** matrix, int i, int j)
  
     while (q <= min) 
     { 
-        double **B = newmatrix(q,q); 
+        double **B = newMatrix (q, q); 
  
-        for (int a = 0; a < (i-(q-1)); a++) 
+        for (int a = 0; a < (i-(q-1)); ++a) 
         {
-            for (int b = 0; b < (j-(q-1)); b++)
+            for (int b = 0; b < (j-(q-1)); ++b)
             {
-                for (int c = 0; c < q; c++)
-                    for (int d = 0; d < q; d++)
+                for (int c = 0; c < q; ++c)
+                    for (int d = 0; d < q; ++d)
                         B[c][d] = matrix[a+c][b+d];
- 
-                if(!(Determinate(B,q) == 0)) 
+                        
+                if(!(determinate(B,q) == 0)) 
                 { 
                     r = q; 
                 }
             }
         }
-        DelMatr(B,q);
+        
+        delMatr (B,q);
     	q++; 
     }
+    
     return r;
 }
 
-void TypeMatr(double **A,int n,int m)
+void typeMatr (double **A, int n, int m)
 {
 	cout << "Type of matrix:";
-	if(n == m) cout << " square";
+	
+	if (n == m) 
+		cout << " square";
+		
 	bool temp = true;
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < m; j++)
-		{
-			if (A[i][j] != 0) temp = false;
-		}
-	}
-	if (temp) cout << " zero";
+	
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+			if (A[i][j] != 0) 
+				temp = false;
+	
+	if (temp)
+		cout << " zero";
+		
 	temp = true;
-	if (m == 1) cout << " vector-column";
-	if (n == 1) cout << " vector-line";
-	for(int i = 0; i < n; i++)
+	
+	if (m == 1) 
+		cout << " vector-column";
+	if (n == 1) 
+		cout << " vector-line";
+	
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+			if (i != j && A[i][j] != 0) 
+				temp = false;
+	
+	if (temp) 
+		cout << " diagonal";
+	
+	temp = true;
+	
+	for (int i = 0; i < n; ++i)
 	{
-		for(int j = 0; j < m; j++)
+		for (int j = 0; j < m; ++j)
 		{
-			if (i != j && A[i][j] != 0) temp = false;
+			if (i != j && A[i][j] != 0) 
+				temp = false;
+			if (i == j && A[i][j] != 1) 
+				temp = false;
 		}
 	}
 	
-	if (temp) cout << " diagonal";
+	if(temp) 
+		cout << " identity";
+		
 	temp = true;
-	for(int i = 0;i < n; i++)
-	{
-		for(int j = 0; j < m; j++)
-		{
-			if(i != j && A[i][j] != 0) temp = false;
-			if(i == j && A[i][j] != 1) temp = false;
-		}
-	}
-	if(temp) cout << " identity";
+	
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+			if (i < j && A[i][j] != 0) 
+				temp = false;
+	
+	if (temp) 
+		cout << " lower diagonal";
+		
 	temp = true;
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < m; j++)
-		{
-			if(i < j && A[i][j] != 0) temp = false;
-		}
-	}
-	if(temp) cout << " lower diagonal";
-	temp = true;
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < m; j++)
-		{
-			if(i > j && A[i][j] != 0) temp = false;
-		}
-	}
-	if(temp) cout << " upper diagonal";
-	temp = true;
+	
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (i > j && A[i][j] != 0) 
+				temp = false;
+				
+	if (temp) 
+		cout << " upper diagonal";
+		
 	cout << endl;
 }
 
-void stoi(string s,int &k)
+void stoi (string s, int &k)
 {
-	k=0;
-	bool flag=1;
-	for(int i=s.size()-1;i>=0 && flag;i--)
+	k = 0;
+	bool flag = 1;
+	
+	for (int i = s.size()-1; i >= 0 && flag; --i)
 	{
-		if(s[i]>='0' && s[i]<='9') k=k*10+(s[i]-'0');
-		else flag=0;
+		if (s[i] >= '0' && s[i] <= '9') 
+			k=k*10+(s[i]-'0');
+		else 
+			flag=0;
 	}
-	if(!flag){
-		cout<<"Incorrect input (int), try again: ";
-		cin>>s;
-		stoi(s,k);
+	
+	if (!flag)
+	{
+		cout << "Incorrect input (int), try again: ";
+		cin >> s;
+		stoi (s, k);
 	}
 }
 
-void stod(string s,double &k)
+void stod (string s, double &k)
 {
-	k=0;
-	bool flag=1;
-	int i=0,count=0;
-	while(i<s.size() && s[i]!='.')
+	k = 0;
+	bool flag = 1;
+	int i = 0, count = 0;
+	
+	while (i < s.size() && s[i] != '.')
 	{
-		if(s[i]>='0' && s[i]<='9') k=k*10+(s[i]-'0');
-		else flag=0;
-		i++;
+		if (s[i] >= '0' && s[i] <= '9') 
+			k = k * 10 + (s[i] - '0');
+		else 
+			flag=0;
+		++i;
 	}
-	i++;
-	while(i<s.size())
+	
+	++i;
+	
+	while (i < s.size())
 	{
-		count++;
-		if(s[i]>='0' && s[i]<='9') k=k*10+(s[i]-'0');
-		else flag=0;
-		i++;
+		++count;
+		if (s[i] >= '0' && s[i] <= '9') 
+			k = k * 10 + (s[i] - '0');
+		else 
+			flag=0;
+		++i;
 	}
-	if(count!=0)
+	
+	if (count != 0)
 	{
-		double ten=1;
-		for(i=0;i<count;i++) ten*=10;
-		k/=ten;
+		double ten = 1;
+		for (i = 0; i < count; ++i) 
+			ten*=10;
+		k /= ten;
 	}
-	if(!flag){
-		cout<<"Incorrect input (double), try again: ";
-		cin>>s;
-		stod(s,k);
+	
+	if (!flag)
+	{
+		cout << "Incorrect input (double), try again: ";
+		cin >> s;
+		stod (s, k);
 	}
 }
