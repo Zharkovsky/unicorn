7e722e0ff4a5b5349c7314fcdba32ebfc8ba1942
AnnaKozlova <zzzyyyyy@mail.ru>
2017-04-15 13:28:54 +0300

    исправленное

--- vyvod.h
+++ vyvod.h
@@ -3,7 +3,16 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-void show (double **, int , int );
+void show (double **a, int n, int m)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        	cout << setw(15) << a[i][j];
+    	cout << endl;
+    }
+ return;
+}
 #endif
 
 
